deea18c5710a9fec2016571a0e58a38e4ead70b3
Maor30Levy <75747578+Maor30Levy@users.noreply.github.com>
2021-07-04 15:00:50 -0700

    Update deploy.sh

--- stages/deploy/deploy.sh
+++ stages/deploy/deploy.sh
@@ -3,3 +3,9 @@
 echo "******************************"
 echo "***** Deploying Project ******"
 echo "******************************"
+
+echo $PROJECT > /tmp/.project_name
+
+scp -T /tmp/.project_name $HOST_USERNAME@$HOST_IP:/tmp/.project_name
+scp -T ./stages/deploy/publish $HOST_USERNAME@$HOST_IP:/tmp/publish
+ssh -T $HOST_USERNAME@$HOST_IP "/tmp/publish"
