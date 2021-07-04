2d50ebb240cfbb9a5a3c3e32a6fcf26d72eaec4e
Maor30Levy <75747578+Maor30Levy@users.noreply.github.com>
2021-07-04 14:25:50 -0700

    Delete fetch directory

--- fetch/fetch.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "******************************"
-echo "*** Fetching Project Files ***"
-echo "******************************"
-
-git clone "https://github.com/Maor30Levy/$PROJECT.git"
