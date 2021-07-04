e8ba46c2a4cb987f05432b2f4155f1c4ec25a7b5
Maor30Levy <75747578+Maor30Levy@users.noreply.github.com>
2021-07-04 14:25:40 -0700

    Create fetch.sh

---
+++ stages/fetch/fetch.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "******************************"
+echo "*** Fetching Project Files ***"
+echo "******************************"
+
+git clone "https://github.com/Maor30Levy/$PROJECT.git"
