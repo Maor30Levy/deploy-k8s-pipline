b0314fc07af057fef59d8b468f85c71e30cd226f
Maor30Levy <75747578+Maor30Levy@users.noreply.github.com>
2021-07-05 00:30:18 -0700

    Update publish.sh

--- stages/deploy/publish.sh
+++ stages/deploy/publish.sh
@@ -3,7 +3,7 @@ export PROJECT=$(sed -n '1p' /tmp/.project_name)
 git clone https://github.com/Maor30Levy/$PROJECT.git
 /tmp/implement-env
 cat ./$PROJECT/templates/env.yaml
-# helm search repo nginx-stable || helm repo add nginx-stable https://helm.nginx.com/stable
-# helm repo update
-# helm install my-release nginx-stable/nginx-ingress
-# helm install $PROJECT $PROJECT/
+helm search repo nginx-stable || helm repo add nginx-stable https://helm.nginx.com/stable
+helm repo update
+helm install my-release nginx-stable/nginx-ingress
+helm install $PROJECT $PROJECT/
