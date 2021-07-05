db536b389bb76ece946ba97869f36f16c30e2358
Maor30Levy <75747578+Maor30Levy@users.noreply.github.com>
2021-07-05 00:19:04 -0700

    Update implement-env.sh

--- stages/deploy/implement-env.sh
+++ stages/deploy/implement-env.sh
@@ -1,2 +1,26 @@
 #/bin/bash
+export CLIENT_PORT=$(sed -n '2p' /tmp/.project_name)
+export SERVER_PORT=$(sed -n '3p' /tmp/.project_name)
+export WORKER_PORT=$(sed -n '4p' /tmp/.project_name)
+export PARSER_PORT=$(sed -n '5p' /tmp/.project_name)
+export REDIS_PORT=$(sed -n '6p' /tmp/.project_name)
+export SERVER_HOST=$(sed -n '7p' /tmp/.project_name)
+export WORKER_HOST=$(sed -n '8p' /tmp/.project_name)
+export PARSER_HOST=$(sed -n '9p' /tmp/.project_name)
+export REDIS_HOST=$(sed -n '10p' /tmp/.project_name)
+export AWS_ACCESS_KEY_ID=$(sed -n '11p' /tmp/.project_name)
+export AWS_SECRET_ACCESS_KEY=$(sed -n '12p' /tmp/.project_name)
+export MONGODB=$(sed -n '13p' /tmp/.project_name)
 
+sed 's/!!CLIENT_PORT!!/$CLIENT_PORT/g' ./$PROJECT/templates/env.yaml
+sed 's/!!SERVER_PORT!!/$SERVER_PORT/g' ./$PROJECT/templates/env.yaml
+sed 's/!!WORKER_PORT!!/$WORKER_PORT/g' ./$PROJECT/templates/env.yaml
+sed 's/!!PARSER_PORT!!/$PARSER_PORT/g' ./$PROJECT/templates/env.yaml
+sed 's/!!REDIS_PORT!!/$REDIS_PORT/g' ./$PROJECT/templates/env.yaml
+sed 's/!!SERVER_HOST!!/$SERVER_HOST/g' ./$PROJECT/templates/env.yaml
+sed 's/!!WORKER_HOST!!/$WORKER_HOST/g' ./$PROJECT/templates/env.yaml
+sed 's/!!PARSER_HOST!!/$PARSER_HOST/g' ./$PROJECT/templates/env.yaml
+sed 's/!!REDIS_HOST!!/$REDIS_HOST/g' ./$PROJECT/templates/env.yaml
+sed 's/!!AWS_ACCESS_KEY_ID!!/$AWS_ACCESS_KEY_ID/g' ./$PROJECT/templates/env.yaml
+sed 's/!!AWS_SECRET_ACCESS_KEY!!/$AWS_SECRET_ACCESS_KEY/g' ./$PROJECT/templates/env.yaml
+sed 's/!!MONGODB!!/$MONGODB/g' ./$PROJECT/templates/env.yaml
